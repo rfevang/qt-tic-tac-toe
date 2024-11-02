23a31bec3154311c6c7ee883cd69208f5897825a
Rune Fevang <rune.fevang@gmail.com>
2024-11-02 20:25:12 +0100

    QRects are x,y,width,height, not x1,y1,x2,y2

--- qtictactoesquare.cpp
+++ qtictactoesquare.cpp
@@ -20,19 +20,19 @@ void QTicTacToeSquare::paintEvent(QPaintEvent* event) {
     int side = std::min(width(), height());
     // Pretend we're always 100x100 when deciding draw coordinates.
     painter.scale(side / 100.0, side / 100.0);
+
     int margin = 15;
-    painter.translate(margin / 2.0, margin / 2.0);
     if (square == TicTacToeBoard::X) {
         QPen pen(Qt::red);
         pen.setWidth(2);
         painter.setPen(pen);
-        painter.drawLine(0, 0, 100 - margin, 100 - margin);
-        painter.drawLine(0, 100 - margin, 100 - margin, 0);
+        painter.drawLine(margin, margin, 100 - margin, 100 - margin);
+        painter.drawLine(margin, 100 - margin, 100 - margin, margin);
     }
     if (square == TicTacToeBoard::O) {
         QPen pen(Qt::green);
         pen.setWidth(2);
         painter.setPen(pen);
-        painter.drawEllipse(QRect(0, 0, 100 - margin, 100 - margin));
+        painter.drawEllipse(QRect(margin, margin, 100 - 2 * margin, 100 - 2 * margin));
     }
 }
