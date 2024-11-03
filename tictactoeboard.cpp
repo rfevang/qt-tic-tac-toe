082823ef2b2dc18317c9ac18413cddb3838399f2
Rune Fevang <rune.fevang@gmail.com>
2024-11-03 21:39:39 +0100

    Deduce whether the game is over or not.

--- tictactoeboard.cpp
+++ tictactoeboard.cpp
@@ -13,7 +13,7 @@ TicTacToeBoard::Player TicTacToeBoard::getSquare(int squareNo) const {
 void TicTacToeBoard::markSquare(int squareNo) {
     if (squareNo < 0 || squareNo >= 9 || board_[squareNo] != NONE) return;
     setSquare_(squareNo, nextToPlay_);
-    setNextToPlay_(nextToPlay_ == X ? O : X);
+    if (!gameOver_) setNextToPlay_(nextToPlay_ == X ? O : X);
 }
 
 void TicTacToeBoard::restartGame() {
@@ -21,17 +21,55 @@ void TicTacToeBoard::restartGame() {
         setSquare_(i, NONE);
     }
     setNextToPlay_(X);
-    emit gameRestarted(NONE);
+    gameOver_ = false;
+    emit gameRestarted();
 }
 
 void TicTacToeBoard::setSquare_(int squareNo, Player value) {
     if (board_[squareNo] == value) return;
     board_[squareNo] = value;
     emit squareChanged(squareNo, value);
+    checkIfGameIsOver_();
 }
 
 void TicTacToeBoard::setNextToPlay_(Player player) {
     if (nextToPlay_ == player) return;
     nextToPlay_ = player;
     emit nextToPlayChanged(player);
+}
+
+void TicTacToeBoard::checkIfGameIsOver_() {
+    // Checks vertical lines, then horizontal, then the two diagonals.
+    // First squares to check for each combination.
+    static const int startSquares[] = {0, 1, 2, 0, 3, 6, 0, 2};
+    // Increment from the previous square to the next square to check for each
+    // combination.
+    static const int increment[] = {3, 3, 3, 1, 1, 1, 4, 2};
+
+    for(int i = 0; i < 8; i++) {
+        Player maybeWinner = board_[startSquares[i]];
+        if (maybeWinner == NONE) continue;
+        bool won = true;
+        for (int j = 1; j < 3; j++) {
+            if (board_[startSquares[i] + j * increment[i]] != maybeWinner) won = false;
+        }
+        if (won) {
+            declareGameOver_(maybeWinner);
+            return;
+        }
+    }
+
+    // Check for a filled board (draw).
+    for (int i = 0; i < 9; i++) {
+        // Still game to be played, even if it may be impossible to win.
+        if (board_[i] == NONE) return;
+    }
+    // All squares owned by a player, declare draw.
+    declareGameOver_(NONE);
+}
+
+void TicTacToeBoard::declareGameOver_(Player winner) {
+    gameOver_ = true;
+    emit gameOver(winner);
+    setNextToPlay_(NONE);
 }
--- tictactoeboard.h
+++ tictactoeboard.h
@@ -8,6 +8,9 @@ class TicTacToeBoard : public QObject
 {
     Q_OBJECT
 public:
+    // Represent one of the players of Tic-Tac-Toe. NONE is used for cases where
+    // there is not a player answer. For instance, NONE is given for ownership
+    // status of square's without an X or O, or as winner status of a tied game.
     enum Player {
         NONE,
         X,
@@ -15,7 +18,7 @@ public:
     };
     TicTacToeBoard();
 
-    // Retrieve the ownership status of the given square.
+    // Retrieves the ownership status of the given square.
     Player getSquare(int squareNo) const;
 
     // Marks the square for the player who's turn it is. If the square is not
@@ -25,23 +28,34 @@ public:
 signals:
     // The ownership status of a square has changed.
     void squareChanged(int squareNo, Player squareOwner);
-    // A new game has started. The previous game was won by the given player.
-    void gameRestarted(Player winner);
+    // A new game has started.
+    void gameRestarted();
     // Which player's move is next has changed.
     void nextToPlayChanged(Player nextToPlay);
+    // The game is over. It was won by the given player.
+    void gameOver(Player player);
 
 public slots:
     // Clears the board, setting up for a new game.
     void restartGame();
 
 private:
+    // Marks the given square as being owned by the given player.
     void setSquare_(int squareNo, Player value);
+    // Updates nextToPlay_ to the specified player.
     void setNextToPlay_(Player player);
+    // Checks if the game is over.
+    void checkIfGameIsOver_();
+    // Declares the game is over, emitting any necessary signals to notify
+    // interested parties.
+    void declareGameOver_(Player winner);
 
     // Current board state.
     std::vector<Player> board_;
     // Which player is next to play.
     Player nextToPlay_;
+    // true if the game is over.
+    bool gameOver_;
 };
 
 #endif // TICTACTOEBOARD_H
