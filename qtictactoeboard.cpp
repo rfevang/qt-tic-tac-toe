2daeb4d218e0f716503fe59ff998e19e67574b31
Rune Fevang <rune.fevang@gmail.com>
2024-11-04 21:45:06 +0100

    Add a couple of checkboxes for computer play

--- qtictactoeboard.cpp
+++ qtictactoeboard.cpp
@@ -3,6 +3,7 @@
 #include "tictactoeboard.h"
 #include <QGridLayout>
 
+#include <QCheckBox>
 #include <QPushButton>
 
 QTicTacToeBoard::QTicTacToeBoard(TicTacToeBoard* board, QWidget* parent)
@@ -12,12 +13,19 @@ QTicTacToeBoard::QTicTacToeBoard(TicTacToeBoard* board, QWidget* parent)
     for(int i = 0; i < 9; i++) {
         layout->addWidget(new QTicTacToeSquare(board, i), i/3, i%3);
     }
+
+    QCheckBox* computerPlayer1 = new QCheckBox("Computer controls player 1", this);
+    layout->addWidget(computerPlayer1, 3, 0, 1, 2);
+    QCheckBox* computerPlayer2 = new QCheckBox("Computer controls player 2", this);
+    layout->addWidget(computerPlayer2, 4, 0, 1, 2);
     QPushButton* restartButton = new QPushButton(tr("&Restart"), this);
-    layout->addWidget(restartButton, 3, 0, 3, 3);
+    layout->addWidget(restartButton, 4, 2, 1, 1);
+
+
 
     connect(restartButton, &QPushButton::clicked, board_, &TicTacToeBoard::restartGame);
 }
 
 QSize QTicTacToeBoard::sizeHint() const {
-    return QSize(400,800);
+    return QSize(400,480);
 }
